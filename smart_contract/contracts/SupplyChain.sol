a8b251fca0604724f74f474b55f760248ea466eb
Dryan <dryanwong@gmail.com>
2021-06-13 05:06:34 +0800

    Functionality to blacklist a provider

--- smart_contract/contracts/SupplyChain.sol
+++ smart_contract/contracts/SupplyChain.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 contract SupplyChain {
     // 0.01 eth for creating a request for tracking
     uint CREATE_FEE = 1e16;
+    address owner;
 
     struct Product {
         address creator;
@@ -15,6 +16,11 @@ contract SupplyChain {
 
     mapping(uint => Product) _products;
     mapping(address => uint) _ownerToRequest;
+    mapping(address => bool) _blacklist;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     modifier productExists(uint _productHash) {
         require(_products[_productHash].creator != address(0), 'product with the given hash does not exist');
@@ -32,6 +38,7 @@ contract SupplyChain {
         string[] memory temp = new string[](_entities.length);
         _products[_productHash] = Product(msg.sender, _entities, 0, temp);
         for (uint i=0; i< _entities.length;i++) {
+            require(!_blacklist[_entities[i]], 'the chosen provider is in the blacklist');
             _products[_productHash].entityIndex[_entities[i]] = i+1;
         }
     }
@@ -49,6 +56,16 @@ contract SupplyChain {
         _products[_productHash].entityIndex[_previousProvider] = 0;
         _products[_productHash].entityIndex[_newProvider] = index;
         _products[_productHash].entities[index - 1] = _newProvider;
+    }
+
+    function blacklistProvider(address _badProvider) public {
+        require(msg.sender == owner, 'only the owner can blacklist a provider');
+        _blacklist[_badProvider] = true;
+    }
+
+    function unblockProvider(address _provider) public {
+        require(msg.sender == owner, 'only the owner can unblock a provider');
+        _blacklist[_provider] = false;
     }
 
     function getData(uint _productHash) public view productExists(_productHash) returns(address, address, string[] memory) {
