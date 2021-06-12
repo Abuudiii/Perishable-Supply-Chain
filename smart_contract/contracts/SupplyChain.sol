2cecfbf0203ce254d038113424cdaca36538057a
Dryan <dryanwong@gmail.com>
2021-06-13 04:32:40 +0800

    Function to change the data provider

--- smart_contract/contracts/SupplyChain.sol
+++ smart_contract/contracts/SupplyChain.sol
@@ -8,6 +8,7 @@ contract SupplyChain {
     struct Product {
         address creator;
         address[] entities;
+        mapping(address => uint) entityIndex;
         uint status;
         string[] productData;
     }
@@ -30,11 +31,24 @@ contract SupplyChain {
         require(_products[_productHash].creator == address(0), 'product with the given hash already exists');
         string[] memory temp = new string[](_entities.length);
         _products[_productHash] = Product(msg.sender, _entities, 0, temp);
+        for (uint i=0; i< _entities.length;i++) {
+            _products[_productHash].entityIndex[_entities[i]] = i+1;
+        }
     }
 
     function updateData(uint _productHash, string memory _data) public productExists(_productHash) correctOrder(_productHash) {
         _products[_productHash].productData[_products[_productHash].status] = _data;
         _products[_productHash].status += 1;
+    }
+
+    function changeProvider(uint _productHash, address _previousProvider, address _newProvider) public productExists(_productHash) {
+        require(msg.sender == _products[_productHash].creator, 'only the creator can change the provider');
+        uint index = _products[_productHash].entityIndex[_previousProvider];
+        require(index != 0, 'unknown previous provider');
+        require(index > _products[_productHash].status, 'already passed that state');
+        _products[_productHash].entityIndex[_previousProvider] = 0;
+        _products[_productHash].entityIndex[_newProvider] = index;
+        _products[_productHash].entities[index - 1] = _newProvider;
     }
 
     function getData(uint _productHash) public view productExists(_productHash) returns(address, address, string[] memory) {
